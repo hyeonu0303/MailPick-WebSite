65a1ace40c7768f80ae877fdf383780cb064951c
hyeonu <hyeonu030334@gmail.com>
2024-02-16 12:43:27 +0900

    fix: MailPick으로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ const Home = ()=> {
     <div className="container mx-auto min-h-screen flex items-center justify-center">
       <div className="flex-col items-center text-center">
         <p className="text-9xl">Smart AI Email</p>
-        <p className="text-3xl info-color pb-5">Mailin은 이메일 이상의 기능을 제공합니다.</p><br/>
+        <p className="text-3xl info-color pb-5">MailPick은 이메일 이상의 기능을 제공합니다.</p><br/>
         <button className="py-3 px-8 mr-2 bg-main-color rounded-full text-white text-2xl p-3">시작하기</button>
         <button className="font-main-color text-2xl p-3">플랜 및 가격</button>
       </div>
